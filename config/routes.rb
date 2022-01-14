cb9c7a10e27f4f50039a2b557363fedb69bc5875
Mesi <mse212002@gmail.com>
2022-01-14 15:48:17 +0200

    :construction: added restaurants page and changed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'restaurants#index'
+  resources :restaurants
   get 'pages/about'
   get 'pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
